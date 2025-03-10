c0aca325278f138732bcbb834e06a86428b155cd
Bekmurodo <zsuhrob240@gmail.com>
2025-03-10 22:54:50 +0300

    Create views.py

---
+++ goodreads/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def landing_page(request):
+    print(request.user.is_authenticated)
+    return render(request, 'landing.html')
