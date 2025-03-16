2e263d23925b02f7320888e3ee4fb69e6927f29d
Bekmurodo <zsuhrob240@gmail.com>
2025-03-16 14:41:20 +0300

    Update urls.py

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -12,3 +13,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('books/', include('books.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
