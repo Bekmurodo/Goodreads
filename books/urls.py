850fc49467db413ff43d998a226a4b21161ffbc6
Bekmurodo <zsuhrob240@gmail.com>
2025-03-25 23:49:32 +0300

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from books.views import BookView, BookDetailView
+from .views import BookView, BookDetailView, add_book
 
 app_name = 'books'
 
@@ -7,4 +7,5 @@ app_name = 'books'
 urlpatterns = [
     path("", BookView.as_view(), name='list'),
     path('<int:id>/', BookDetailView.as_view(), name='detail'),
+    path('add/', add_book, name='add_book'),
 ]
