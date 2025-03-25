0b40a4fb217ee36eb915fa16c67f71c90964c12f
Bekmurodo <zsuhrob240@gmail.com>
2025-03-25 23:49:36 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView, DetailView
-
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
 from.models import Book
+from .forms import BookAddForm
+from django.views import View
 
-# class BookView(View):
-#     def get(self, request):
-#         books = Book.objects.all()
-#         context = {
-#             'books': books
-#         }
-#         return render(request, 'books/list.html', context)
+class BookView(View):
+    def get(self, request):
+        books = Book.objects.all().order_by('id')
+        paginator = Paginator(books, 2)
+
+        page_num = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_num)
+
+        context = {
+            'page_obj': page_obj
+        }
+        return render(request, 'books/list.html', context)
 
 
-class BookView(ListView):
-    template_name = 'books/list.html'
-    queryset = Book.objects.all()
-    context_object_name = 'books'
+# class BookView(ListView):
+#     template_name = 'books/list.html'
+#     queryset = Book.objects.all()
+#     context_object_name = 'books'
 
 
 class BookDetailView(DetailView):
@@ -29,3 +35,17 @@ class BookDetailView(DetailView):
 #     def get(self, request, id):
 #         book = Book.objects.get(id=id)
 #         return render(request, 'books/detail.html', {'book': book})
+#
+
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books:list')  # Перенаправление на страницу со списком книг
+    else:
+        form = BookAddForm()
+
+    return render(request, 'books/add.html', {'form': form})
+
