3cb8f7d162472512419f5a57a07d26afe6135f78
Bekmurodo <zsuhrob240@gmail.com>
2025-04-01 23:26:46 +0300

    Update forms.py

--- books/forms.py
+++ books/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Book
+from .models import Book, BookReview
+
 
 class BookAddForm(forms.ModelForm):
 
     class Meta:
         model = Book
-        fields = ['title', 'description', 'isbn', 'cover_picture']
+        fields = ('title', 'description', 'isbn', 'cover_picture')
+
+class BookReviewForm(forms. ModelForm):
+    class Meta:
+        model = BookReview
+        fields = ('stars_given', 'comment')
