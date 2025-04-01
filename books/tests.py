fed1a1a1e7dcbe92a2e4b528ff98b2d425491848
Bekmurodo <zsuhrob240@gmail.com>
2025-04-01 23:26:49 +0300

    Update tests.py

--- books/tests.py
+++ books/tests.py
@@ -10,15 +10,20 @@ class BooksTestCase(TestCase):
         self.assertContains(response, "No books found")
 
     def test_book_list(self):
-        Book.objects.create(title='Book1', description='Description1', isbn='1111111')
-        Book.objects.create(title='Book2', description='Description2', isbn='2222')
-        Book.objects.create(title='Book3', description='Description3', isbn='1111222111')
+        book1 = Book.objects.create(title='Book1', description='Description1', isbn='1111111')
+        book2 = Book.objects.create(title='Book2', description='Description2', isbn='2222')
+        book3 = Book.objects.create(title='Book3', description='Description3', isbn='1111222111')
 
-        response = self.client.get(reverse('books:list'))
+        response = self.client.get(reverse('books:list') + '?page_size=2')
 
         books = Book.objects.all()
-        for book in books:
+        for book in (book1, book2):
            self.assertContains(response, book.title)
+           self.assertNotContains(response, book3.title)
+
+        response = self.client.get(reverse('books:list') + '?page=2&page_size=2')
+
+        self.assertContains(response, book3.title)
 
     def test_detail_page(self):
         book = Book.objects.create(title='Book1', description='Description1', isbn='1111111')
@@ -27,3 +32,28 @@ class BooksTestCase(TestCase):
 
         self.assertContains(response, book.title)
         self.assertContains(response, book.description)
+
+    def test_search_book(self):
+        book1 = Book.objects.create(title='Sport', description='Description1', isbn='1111111')
+        book2 = Book.objects.create(title='Guide', description='Description2', isbn='2222')
+        book3 = Book.objects.create(title='Travel', description='Description3', isbn='1111222111')
+
+        response = self.client.get(reverse('books:list') + '?q=sport')
+
+        self.assertContains(response, book1.title)
+        self.assertNotContains(response, book2.title)
+        self.assertNotContains(response, book3.title)
+
+        response = self.client.get(reverse('books:list') + '?q=guide')
+
+        self.assertContains(response, book2.title)
+        self.assertNotContains(response, book1.title)
+        self.assertNotContains(response, book3.title)
+
+        response = self.client.get(reverse('books:list') + '?q=travel')
+
+        self.assertContains(response, book3.title)
+        self.assertNotContains(response, book2.title)
+        self.assertNotContains(response, book1.title)
+
+
