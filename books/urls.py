5f8ba8cd77f9388c5e186e0755d2908ad404e0ab
Bekmurodo <zsuhrob240@gmail.com>
2025-04-24 17:30:23 +0300

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import BookView, BookDetailView, add_book, AddReviewView
+from .views import (BookView,
+                    BookDetailView,
+                    add_book, AddReviewView,
+                    EditReviewView,
+                    ConfirmDeleteReviewView,
+                    DeleteReviewView
+                    )
 
 app_name = 'books'
 
@@ -9,4 +15,12 @@ urlpatterns = [
     path('<int:id>/', BookDetailView.as_view(), name='detail'),
     path('<int:id>/reviews/', AddReviewView.as_view(), name='reviews'),
     path('add/', add_book, name='add_book'),
+    path("<int:book_id>/reviews/<int:review_id>/edit/", EditReviewView.as_view(), name='edit-review'),
+    path("<int:book_id>/reviews/<int:review_id>/confirm/delete/",
+         ConfirmDeleteReviewView.as_view(),
+         name='confirm-delete-review'),
+    path("<int:book_id>/reviews/<int:review_id>/delete/",
+         DeleteReviewView.as_view(),
+         name='delete-review'
+         ),
 ]
