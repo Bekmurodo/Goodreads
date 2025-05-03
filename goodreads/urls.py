5798c04694cab3775e9ac4c041d66fc161aec655
Bekmurodo <zsuhrob240@gmail.com>
2025-05-03 23:47:55 +0300

    Update urls.py

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     path("", landing_page, name='landing_page'),
     path("home/", home_page, name='home_page'),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 
     path('users/', include('users.urls')),
-    path('books/', include('books.urls'))
+    path('books/', include('books.urls')),
+    path('api/', include('api.urls'))
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
